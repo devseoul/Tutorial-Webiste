2ae870c368517c7bbdfe3356e6432847c82c8a09
devseoul <lukas.diekmann@smail.th-koeln.de>
2024-11-15 14:38:05 +0100

    pls deploy

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,9 +2,3 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles.css");
 };
 
-module.exports = {
-    dir: {
-        input: "src",
-        output: "docs"
-    }
-};
