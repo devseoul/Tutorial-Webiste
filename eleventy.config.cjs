039ddadb47ac5af42155157f058220bbae9ad4c9
devseoul <114822601+devseoul@users.noreply.github.com>
2024-11-17 23:14:32 +0100

    Update eleventy.config.cjs

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -3,6 +3,12 @@ module.exports = function(eleventyConfig) {
 };
 
 module.exports = function(eleventyConfig) {
+  return {
+    pathPrefix: "/Tutorial-Webiste/",
+  };
+};
+
+module.exports = function(eleventyConfig) {
     return {
         dir: {
             output: "_site" // Sicherstellen, dass der Output-Ordner korrekt ist
